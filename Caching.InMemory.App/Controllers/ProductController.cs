ef59069576ace0b2585dccbd391af6e54f68d6d1
ikbalkazanc <60016655+ikbalkazanc@users.noreply.github.com>
2021-01-21 05:33:55 +0300

    Add project files.

---
+++ Caching.InMemory.App/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caching.InMemory.App.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("Time",DateTime.Now.ToString());
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.Time = _memoryCache.Get<string>("Time");
+            return View();
+        }
+    }
+}
